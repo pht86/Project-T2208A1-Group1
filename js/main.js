e153a56eb9648ac0321a83af70bdf4cdd1e4d2e8
pht86 <114332758+pht86@users.noreply.github.com>
2022-12-27 09:58:29 +0700

    create pages

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@
 window.onload = function(){
     document.querySelector("#preloader").style.display = "none";
 }
+
+//Go to top
+let mybutton = document.getElementById("btnGoTop");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function GoTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
