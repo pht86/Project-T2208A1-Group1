f6cf3bcbaa5efeb966f7acd4d5b7b537af7ce498
pht86 <114332758+pht86@users.noreply.github.com>
2022-12-28 15:22:51 +0700

    Posts Pages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 //Hide the preloader
-window.onload = function(){
+window.onload = function () {
     document.querySelector("#preloader").style.display = "none";
 }
 
 //Go to top
 let mybutton = document.getElementById("btnGoTop");
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
@@ -13,7 +16,24 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
+
 function GoTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+//Tab Content
+
+function showTab(evt, tabId) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabId).style.display = "block";
+    evt.currentTarget.className += " active";
 }
